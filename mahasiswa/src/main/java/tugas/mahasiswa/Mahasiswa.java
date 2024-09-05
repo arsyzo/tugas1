b607dd654bc8d27d97ee9d9ee83fa19ffa8a6ae0
arsyzo <168258257+arsyzo@users.noreply.github.com>
2024-09-05 11:58:03 +0700

    Add files via upload

---
+++ mahasiswa/src/main/java/tugas/mahasiswa/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tugas.mahasiswa;
+
+/**
+ *
+ * @author Marsy
+ */
+public class Mahasiswa {
+    void membaca(){
+        System.out.println("Mahasiswa sedang membaca.");
+    }
+    void nyontek(){
+        System.out.println("Mahasiswa menyontek saat ujian.");
+    }
+    void modifikasi(){
+        System.out.println("Mahasiswa sedang belajar memodifikasi tugas.");
+    }
+}
